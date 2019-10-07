6599ca242da270ea2cdf780366f7ff9d0ca60eef
root <root@localhost.localdomain>
2019-10-07 20:04:09 +0300

    025 React компоненты

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//const el = React.createElement('h1', null, 'Hello World!');
-//const el = <h1>Hello World</h1>;
-//console.log(el);
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  );
+};
 
-const el = (
+const AppHeader = () => {
+  return (
+    <h1>My Todo List</h1>
+  );
+};
+
+const SearchPanel = () => {
+  return (
+    <input placeholder="search" />
+  );
+};
+
+const App = () => {
+  return (
     <div>
-        <h1>My Todo List</h1>
-        <input placeholder="search" />
-        <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
-        </ul>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
     </div>
-);
+  );
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
