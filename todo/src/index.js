e3ca41cf7bc3160a3a5ce9cfdafc831a5b3e6f6a
root <root@localhost.localdomain>
2019-10-07 19:50:55 +0300

    024 React элементы

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,19 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//const el = React.createElement('h1', null, 'Hello World!');
+//const el = <h1>Hello World</h1>;
+//console.log(el);
+
+const el = (
+    <div>
+        <h1>My Todo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    </div>
+);
+
+ReactDOM.render(el, document.getElementById('root'));
