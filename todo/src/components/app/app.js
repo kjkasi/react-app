25a90ab591ec025f2944bb36e2520d180025303c
root <root@localhost.localdomain>
2019-10-09 19:52:14 +0300

    041 setState() - добавление элемента

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -4,9 +4,13 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
+
 import './app.css';
 
 export default class App extends Component {
+
+  maxId = 100;
 
   state = {
     todoData: [
@@ -33,6 +37,25 @@ export default class App extends Component {
 
   };
 
+  AddItem = (text) => {
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    };
+
+    this.setState(( {todoData }) => {
+      const newArray = [
+        ...todoData,
+        newItem
+      ]
+
+      return{
+        todoData: newArray
+      };
+    });
+  };
+
   render() {
     return (
       <div className="todo-app">
@@ -45,6 +68,8 @@ export default class App extends Component {
         <TodoList 
           todos= { this.state.todoData } 
           onDeleted={ this.deleteItem } />
+        <ItemAddForm 
+          onItemAdded={ this.AddItem } />
       </div>
     );
   };
