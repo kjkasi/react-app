c9b0f6850d55d512d303fae80bbf854aedee26c0
root <root@localhost.localdomain>
2019-10-12 11:52:42 +0300

    053 Обработка ошибок в Fetch API

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,18 +1,17 @@
 const getResourse = async(url) => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${ url }, received ${ res.status }`); 
+  };
+
   const body = await res.json();
   return body;
 };
 
-getResourse('https://swapi.co/api/people/1/')
+getResourse('https://swapi.co/api/people/1123123421/')
   .then((body) => {
     console.log(body);
+  }).catch((err) => {
+    console.error('Could not fetch', err);
   });
-
-/*
-fetch('https://swapi.co/api/people/1/').then((res) => {
-  return res.json();
-}).then((body) => {
-  console.log(body);
-});
-*/
