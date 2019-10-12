d4f4a59d9748ae544c4c45e6526e430b7c093576
root <root@localhost.localdomain>
2019-10-12 11:36:29 +0300

    052 Как работает Fetch API

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,3 +1,18 @@
-//https://swapi.co
+const getResourse = async(url) => {
+  const res = await fetch(url);
+  const body = await res.json();
+  return body;
+};
 
-console.log('Hello word');
+getResourse('https://swapi.co/api/people/1/')
+  .then((body) => {
+    console.log(body);
+  });
+
+/*
+fetch('https://swapi.co/api/people/1/').then((res) => {
+  return res.json();
+}).then((body) => {
+  console.log(body);
+});
+*/
