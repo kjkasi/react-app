2855d8611a2f4c1c558d2fb027513295196a2cd8
root <root@localhost.localdomain>
2019-10-13 21:40:01 +0300

    057 Трансформация данных API

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -9,11 +9,7 @@ export default class RandomPlanet extends Component {
   SwapiService = new SwapiService();
 
   state = {
-    id: null,
-    name: null,
-    population: null,
-    rotationPeriod: null,
-    diameter: null
+    planet: {}
   };
 
   constructor() {
@@ -21,24 +17,20 @@ export default class RandomPlanet extends Component {
     this.updatePlanet();
   };
 
+  onPlanetLoaded = (planet) => {
+    this.setState({planet});
+  };
+
   updatePlanet() {
     const id = Math.floor(Math.random()*25 + 2);
     this.SwapiService
       .getPlanet(id)
-      .then((planet) => {
-        this.setState({
-          id,
-          name: planet.name,
-          population: planet.population,
-          rotationPeriod: planet.orbital_period,
-          diameter: planet.diameter
-        });
-      });
+      .then(this.onPlanetLoaded);
   };
 
   render() {
 
-    const { id, name, population, rotationPeriod, diameter } = this.state;
+    const { planet:{ id, name, population, rotationPeriod, diameter }} = this.state;
 
     return (
       <div className="random-planet jumbotron rounded">
