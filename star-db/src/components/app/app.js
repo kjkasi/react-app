87bec70377e7b33352e269b7e7fe4aafb56f66cd
root <root@localhost.localdomain>
2019-10-14 21:17:35 +0300

    070 Используем componentDidCatch() на практике

--- star-db/src/components/app/app.js
+++ star-db/src/components/app/app.js
@@ -2,11 +2,9 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
-
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator';
+import PeoplePage from '../people-page';
 
 import './app.css';
 
@@ -14,7 +12,6 @@ export default class App extends Component {
 
   state = {
     showRandomPlanet: true,
-    selectedPerson: 1,
     hasError: false
   };
 
@@ -26,31 +23,21 @@ export default class App extends Component {
     });
   };
 
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    });
-  };
-
   componentDidCatch() {
-    console.log('componentDidCatch()');
-    this.setState({
-      hasError: true
-    });
-  };
+    this.setState({ hasError: true });
+  }
 
   render() {
 
     if (this.state.hasError) {
       return <ErrorIndicator />
-    };
+    }
 
     const planet = this.state.showRandomPlanet ?
       <RandomPlanet/> :
       null;
 
     return (
-
       <div className="stardb-app">
         <Header />
         { planet }
@@ -64,16 +51,12 @@ export default class App extends Component {
           <ErrorButton />
         </div>
 
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList 
-              onItemSelected= { this.onPersonSelected } />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails 
-              personId= { this.state.selectedPerson }/>
-          </div>
-        </div>
+        <PeoplePage />
+
+        <PeoplePage />
+
+        <PeoplePage />
+
       </div>
     );
   }
--- star-db/src/components/item-list/item-list.js
+++ star-db/src/components/item-list/item-list.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 import './item-list.css';
-import SwapiService from '../../services/swapi-service';
-import Spinner from '../spinner/';
+import SwapiService from "../../services/swapi-service";
+import Spinner from "../spinner/spinner";
 
 export default class ItemList extends Component {
 
@@ -13,40 +13,39 @@ export default class ItemList extends Component {
   };
 
   componentDidMount() {
-    this.swapiService
-      .getAllPeople()
+    this.swapiService.getAllPeople()
       .then((peopleList) => {
         this.setState({
           peopleList
         });
       });
-  };
+  }
 
   renderItems(arr) {
-    return arr.map(({ id, name }) => {
+    return arr.map(({id, name}) => {
       return (
         <li className="list-group-item"
-          key={ id }
-          onClick= { () => this.props.onItemSelected(id) } >
-          { name }
+            key={id}
+            onClick={() => this.props.onItemSelected(id)}>
+          {name}
         </li>
       );
-    }); 
-  };
+    });
+  }
 
   render() {
 
     const { peopleList } = this.state;
 
     if (!peopleList) {
-      return <Spinner />
+      return <Spinner />;
     }
 
     const items = this.renderItems(peopleList);
 
     return (
       <ul className="item-list list-group">
-        { items }
+        {items}
       </ul>
     );
   }
