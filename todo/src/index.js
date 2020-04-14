8928615618917eb274106f9998922970fc8079d3
root <root@localhost.localdomain>
2020-04-14 10:51:42 +0300

    5. JSX

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const TodoList = () => {
+
+  const items = ['Learn React', 'Build Awesome App'];
+
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
+      <li>{ items[0] }</li>
+      <li>{ items[1] }</li>
     </ul>
   )
 }
@@ -17,12 +20,32 @@ const AppHeader = () => {
 }
 
 const SearchPanel = () => {
-  return <input placeholder='search' />
+  const searchText = 'Type here to search';
+  const searchStyle = {
+    fontSize: '25px'
+  }
+  return <input placeholder={ searchText }
+                className="foo"
+                htmlFor=""
+                style={searchStyle} />
 }
 
 const App = () => {
+
+  /* const isLoggedIn = false;
+  const loginBox = <span>Log in please</span>;
+  const nullBox = null;
+  const welcomeBox = <span>Welcome Back</span>; */
+
   return (
     <div>
+      {/* <span>{ (new Date()).toString() }</span>
+      { loginBox }
+      { nullBox } 
+      { isLoggedIn ? null : loginBox }
+      { isLoggedIn ? welcomeBox : loginBox }
+      { true } 
+      { false } */}
       <AppHeader />
       <SearchPanel />
       <TodoList />
