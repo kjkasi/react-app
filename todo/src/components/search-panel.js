c2aa312c906023538f2137cec7050778b14841d3
root <root@localhost.localdomain>
2020-04-15 13:40:20 +0300

    11. Как импортировать css

--- todo/src/components/search-panel.js
+++ todo/src/components/search-panel.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-const SearchPanel = () => {
-  const searchText = 'search';
-  const searchStyle = {
-    fontSize: '25px'
-  }
-  return <input placeholder={ searchText }
-                className="foo"
-                htmlFor=""
-                style={searchStyle} />
-}
+import './search-panel.css';
 
-export default SearchPanel
+const SearchPanel = () => {
+  return (
+    <input type="text"
+              className="form-control search-input"
+              placeholder="type to search" />
+  );
+};
+
+export default SearchPanel;
